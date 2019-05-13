d2517458ec3b4a934e235a9d8429f5ce9d451e05
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-05-13 06:10:22 +0000

    Add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const exec = require('child_process').exec;
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 function execute(commands) {
   exec(commands.join(';'), (err, stdout, stderr) => {
     if (stdout) process.stdout.write(stdout);
@@ -23,28 +25,32 @@ module.exports = env => {
         }
       ]
     },
-    plugins: [{
-      apply: (compiler) => {
-        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
-          if (!env) {
-            execute(['echo "Environment was not specified. Add --env.commit to commit"']);
-            return;
-          }
-          
-          if (env.commit) {
-            execute([
-              'echo "-------- Github Pages --------"',
-              'cd ./Builds',
-              'git add *',
-              `git commit -m "Build ${new Date().toString()}"`,
-              'cd -',
-              'echo ""',
-              'echo "Please push to origin after this build"',
-              'echo "-------- Github Pages --------"'
-            ]);
-          }
-        });
+    plugins: [
+      new HtmlWebpackPlugin(),
+      {
+        apply: (compiler) => {
+          compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+            if (!env) {
+              execute(['echo "Environment was not specified. Add --env.commit to commit"']);
+              return;
+            }
+
+            if (env.commit) {
+              execute([
+                'echo "-------- Github Pages --------"',
+                'cd ./Builds',
+                'git add *',
+                `git commit -m "Build ${new Date().toString()}"`,
+                'git push origin gh-pages',
+                'cd -',
+                'echo ""',
+                'echo "Please push to origin after this build"',
+                'echo "-------- Github Pages --------"'
+              ]);
+            }
+          });
+        }
       }
-    }]
+    ]
   };
 };

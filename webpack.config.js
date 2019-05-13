c9751bf5b583bd44cd6e9c91567774ff25e34fd8
hnOsmium0001 <36975818+hnOsmium0001@users.noreply.github.com>
2019-05-13 04:15:55 +0000

    Basic Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const exec = require('child_process').exec;
+
+function execute(commands) {
+  exec(commands.join(';'), (err, stdout, stderr) => {
+    if (stdout) process.stdout.write(stdout);
+    if (stderr) process.stderr.write(stderr);
+  });
+}
+
+module.exports = env => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './Builds')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    plugins: [{
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+          if (!env) {
+            execute(['echo "Environment was not specified. Add --env.commit to commit"']);
+            return;
+          }
+          
+          if (env.commit) {
+            execute([
+              'echo "-------- Github Pages --------"',
+              'cd ./Builds',
+              'git add *',
+              `git commit -m "Build ${new Date().toString()}"`,
+              'cd -',
+              'echo ""',
+              'echo "Please push to origin after this build"',
+              'echo "-------- Github Pages --------"'
+            ]);
+          }
+        });
+      }
+    }]
+  };
+};
